57e538880a8c6ecf4d410beb7a5b12202b4e2951
JMACBigMan <jemclaurin@gmail.com>
2020-06-30 19:56:24 -0400

    Updates

---
+++ precache-manifest.bc0c57a00652db48670891a3757ffdd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aef0480b5990f4dd859bc4a86940c36",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "da457a2b5f6e6cb9e586",
+    "url": "/game/static/css/main.8b573827.chunk.css"
+  },
+  {
+    "revision": "04c6e66c9ae6cef84f0e",
+    "url": "/game/static/js/2.e8a30c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game/static/js/2.e8a30c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da457a2b5f6e6cb9e586",
+    "url": "/game/static/js/main.da3754c3.chunk.js"
+  },
+  {
+    "revision": "236fc4c89c7ae8f75e1a",
+    "url": "/game/static/js/runtime-main.91027deb.js"
+  }
+]);
